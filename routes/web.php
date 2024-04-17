bbd948cd633e8c44c414569a736a5ec83f670bba
Marino01 <81763040+Marino01@users.noreply.github.com>
2024-04-17 13:07:43 +0200

    week2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/student', function () {
+//     return view('student', ['naam' => 'Sara']);
+//     });
+
+Route::get('/student/{naamUitUrl}',[StudentController::class, 'show']);
